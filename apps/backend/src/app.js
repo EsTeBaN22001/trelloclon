a0c9850584d38d7348261fedc074207656a69655
Esteban <esteban1.redon2@gmail.com>
2025-03-05 20:29:35 -0300

    [backend] add middleware to serve static files from the frontend. fix routing problemas

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -15,17 +15,19 @@ const __dirname = path.dirname(__filename)
 
 export const app = express()
 
-app.use(express.static(path.join(__dirname, '../../', 'frontend/dist/trelloclon-frontend')))
+// Middleware para servir los archivos estáticos del frontend
+const frontendPath = path.join(__dirname, '../../', 'frontend/dist/trelloclon-frontend')
+app.use(express.static(frontendPath))
 
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors())
 
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../', 'frontend/dist/trelloclon-frontend'))
-})
-
 app.use('/api/auth', authRoutes)
 app.use('/api/boards', boardRoutes)
 app.use('/api/lists', listRoutes)
 app.use('/api/cards', cardRoutes)
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'))
+})
